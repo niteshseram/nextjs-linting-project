b70b2a0fa1af16cf6da495acdc9d50a60462aa56
S Nitesh Singh <niteshseram@gmail.com>
2021-09-26 18:15:59 +0530

    Step 3: add custom directory support for ESLint

---
+++ abc/index.js
@@ -0,0 +1,8 @@
+//declare a variable but not used it anywhere in this file
+//should be caught by ESLint after abc directory being added in next.config.js
+const abc=1
+const Home = () => (
+  <div>Hell\o</div>
+)
+
+export default Home
